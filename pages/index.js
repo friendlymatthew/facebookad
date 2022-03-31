5a72d45ddc020b0a1627c79f33c114986f4d3b6b
matthew <matthew@matthews-MacBook-Pro-2.local>
2022-03-31 10:01:09 -0400

    updated syntax css errors

--- pages/index.js
+++ pages/index.js
@@ -324,30 +324,30 @@ export default function Viz() {
 												{bidenSort ? (
 													<svg
 														xmlns="http://www.w3.org/2000/svg"
-														class="h-6 w-6"
+														className="h-6 w-6"
 														fill="none"
 														viewBox="0 0 24 24"
 														stroke="currentColor"
-														stroke-width="2"
+														strokeWidth="2"
 													>
 														<path
-															stroke-linecap="round"
-															stroke-linejoin="round"
+															strokeLinecap="round"
+															strokeLinejoin="round"
 															d="M3 4h13M3 8h9m-9 4h9m5-4v12m0 0l-4-4m4 4l4-4"
 														/>
 													</svg>
 												) : (
 													<svg
 														xmlns="http://www.w3.org/2000/svg"
-														class="h-6 w-6"
+														className="h-6 w-6"
 														fill="none"
 														viewBox="0 0 24 24"
 														stroke="currentColor"
-														stroke-width="2"
+														strokeWidth="2"
 													>
 														<path
-															stroke-linecap="round"
-															stroke-linejoin="round"
+															strokeLinecap="round"
+															strokeLinejoin="round"
 															d="M3 4h13M3 8h9m-9 4h6m4 0l4-4m0 0l4 4m-4-4v12"
 														/>
 													</svg>
@@ -362,30 +362,30 @@ export default function Viz() {
 											{trumpSort ? (
 												<svg
 													xmlns="http://www.w3.org/2000/svg"
-													class="h-6 w-6"
+													className="h-6 w-6"
 													fill="none"
 													viewBox="0 0 24 24"
 													stroke="currentColor"
-													stroke-width="2"
+													strokeWidth="2"
 												>
 													<path
-														stroke-linecap="round"
-														stroke-linejoin="round"
+														strokeLinecap="round"
+														strokeLinejoin="round"
 														d="M3 4h13M3 8h9m-9 4h9m5-4v12m0 0l-4-4m4 4l4-4"
 													/>
 												</svg>
 											) : (
 												<svg
 													xmlns="http://www.w3.org/2000/svg"
-													class="h-6 w-6"
+													className="h-6 w-6"
 													fill="none"
 													viewBox="0 0 24 24"
 													stroke="currentColor"
-													stroke-width="2"
+													strokeWidth="2"
 												>
 													<path
-														stroke-linecap="round"
-														stroke-linejoin="round"
+														strokeLinecap="round"
+														strokeLinejoin="round"
 														d="M3 4h13M3 8h9m-9 4h6m4 0l4-4m0 0l4 4m-4-4v12"
 													/>
 												</svg>
@@ -400,30 +400,30 @@ export default function Viz() {
 												{netSort ? (
 													<svg
 														xmlns="http://www.w3.org/2000/svg"
-														class="h-6 w-6"
+														className="h-6 w-6"
 														fill="none"
 														viewBox="0 0 24 24"
 														stroke="currentColor"
-														stroke-width="2"
+														strokeWidth="2"
 													>
 														<path
-															stroke-linecap="round"
-															stroke-linejoin="round"
+															strokeLinecap="round"
+															strokeLinejoin="round"
 															d="M3 4h13M3 8h9m-9 4h9m5-4v12m0 0l-4-4m4 4l4-4"
 														/>
 													</svg>
 												) : (
 													<svg
 														xmlns="http://www.w3.org/2000/svg"
-														class="h-6 w-6"
+														className="h-6 w-6"
 														fill="none"
 														viewBox="0 0 24 24"
 														stroke="currentColor"
-														stroke-width="2"
+														strokeWidth="2"
 													>
 														<path
-															stroke-linecap="round"
-															stroke-linejoin="round"
+															strokeLinecap="round"
+															strokeLinejoin="round"
 															d="M3 4h13M3 8h9m-9 4h6m4 0l4-4m0 0l4 4m-4-4v12"
 														/>
 													</svg>
