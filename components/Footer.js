534cb72ca0cfc1d6c87dc3026bcf1cd4ec574a56
matthew <matthew@matthews-MacBook-Pro-2.local>
2022-04-01 09:07:59 -0400

    updated footer and index

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,15 @@
-
-
 export default function Footer() {
-
-
-    return (
-        <div className="h-16 bg-slate-800 text-white font-extrabold">
-           <a href="https://github.com/mostvaluableshipfriendship" target="_blank" rel="noreferrer">
-            <div className="text-center py-4 opacity-0 hover:opacity-100 cursor-pointer transition ease-in duration-200">
-                github.com/mostvaluableshipfriendship
-            </div>
-            </a>
-        </div>
-    )
+	return (
+		<div className="h-16 bg-slate-800 text-white grid place-items-center font-extrabold">
+			<a
+				href="https://github.com/mostvaluableshipfriendship"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<div className="text-center opacity-0 hover:opacity-100 cursor-pointer transition ease-in duration-200">
+					github.com/matthewkim0/facebookad
+				</div>
+			</a>
+		</div>
+	);
 }
